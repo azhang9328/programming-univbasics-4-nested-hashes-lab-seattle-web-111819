c359ba4abac2b41a7ea0ba2409b69563e0796b3b
Allen Zhang <azhang9328@gmail.com>
2019-11-17 04:39:56 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -15,9 +15,7 @@ def hopper
           :languages => ["C"]
         }
      }
-programmer_hash.delete(:alan_kay)
-programmer_hash.delete(:dennis_ritchie)
-return programmer_hash
+programmer_hash[:grace_hopper]
 end
 
 def alan_kay_is_known_for
